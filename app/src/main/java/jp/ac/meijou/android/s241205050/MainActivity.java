d274454b39ac42743174087639d26006f30d0724
seito_kasano <241205050@ccmailg.meijo-u.ac.jp>
2025-09-02 13:30:46 +0900

    テキストの演習

--- app/src/main/java/jp/ac/meijou/android/s241205050/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s241205050/MainActivity.java
@@ -1,6 +1,10 @@
 package jp.ac.meijou.android.s241205050;
 
 import android.os.Bundle;
+import android.service.autofill.TextValueSanitizer;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,45 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import jp.ac.meijou.android.s241205050.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+        });
+
+        binding.text.setText(R.string.text_1);
+
+        binding.button.setOnClickListener(view -> {
+            binding.text.setText(R.string.text_sample);
+        });
+
+        binding.editTextText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                binding.text.setText(editable.toString());
+            }
         });
     }
 }
